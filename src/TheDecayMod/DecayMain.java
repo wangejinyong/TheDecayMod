058243cf138e681271fa3fbd6a1e671b6230cdd2
wangejinyong <2473297844@qq.com>
2024-06-17 20:49:35 +0800

    Changes

--- src/TheDecayMod/DecayMain.java
+++ src/TheDecayMod/DecayMain.java
@@ -9,21 +9,7 @@ import mindustry.ui.dialogs.*;
 public class DecayMain extends Mod{
 
     public DecayMain(){
-        Log.info("Loaded TheDecayMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("my_BGL");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("Decay-mod-my_BGL")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-    }
+        Log.info("Loaded TheDecayMod constructor.");}
 
     @Override
     public void loadContent(){
