144e328c09b9bb1bbf29f80836965bfe897fff58
wangejinyong <2473297844@qq.com>
2024-06-21 21:48:42 +0800

    Changes

--- src/TheDecayMod/content/ModBlocks.java
+++ src/TheDecayMod/content/ModBlocks.java
@@ -17,42 +17,41 @@ public class ModBlocks {
     //load the blocks
     public static Block
             //producer
-    energyContainerCreator,energyContainerCharger
-
+    energyContainerCreator,energyContainerCharger,energyContainerConsumer
             ;
 
     public static void load(){
         energyContainerCreator=new AttributeCrafter("energy container creator")
         {{
             requirements(Category.crafting,with(Items.copper,60,Items.lead,30,Items.titanium,15));
-            this.hasItems = true;
-            this.hasLiquids = false;
-            this.hasPower = true;
-            this.itemCapacity = 20;
+            hasItems = true;
+            hasLiquids = false;
+            hasPower = true;
+            itemCapacity = 20;
             //wait for editing
-            this.craftEffect = Fx.smeltsmoke;
-            this.outputItem = new ItemStack(emptyEnergyContainers,1);
-            this.craftTime = 60f;
-            this.size = 2;
+            craftEffect = Fx.smeltsmoke;
+            outputItem = new ItemStack(emptyEnergyContainers,1);
+            craftTime = 60f;
+            size = 2;
             //wait for editing
-            this.ambientSound = Sounds.smelter;
-            this.ambientSoundVolume = 0.06f;
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.06f;
 
             consumePower(1.60f);
         }};
 
-        energyContainerCharger=new ConsumeGenerator("energy container charger")
+        energyContainerConsumer=new ConsumeGenerator("energy container consumer")
         {{
             requirements(Category.power,with(Items.copper,60,Items.lead,30,Items.titanium,15));
-            this.hasItems = true;
-            this.hasLiquids = false;
-            this.hasPower = true;
-            this.itemCapacity = 20;
+            hasItems = true;
+            hasLiquids = false;
+            hasPower = true;
+            itemCapacity = 20;
             //wait for editing
-            this.generateEffect = Fx.generatespark;
-            this.powerProduction = 2f;
-            this.itemDuration = 60f;
-            this.size = 2;
+            generateEffect = Fx.generatespark;
+            powerProduction = 2f;
+            itemDuration = 60f;
+            size = 2;
             //wait for editing
             ambientSound = Sounds.smelter;
             ambientSoundVolume = 0.06f;
