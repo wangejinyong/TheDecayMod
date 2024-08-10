704d5ebaaa76dfa4070cbfe8c5da6758079e6727
wangejinyong <2473297844@qq.com>
2024-08-10 23:55:04 +0800

    Changes

--- src/TheDecayMod/ModModules/RandomForWind.java
+++ src/TheDecayMod/ModModules/RandomForWind.java
@@ -5,10 +5,26 @@ import java.util.Random;
 
 public class RandomForWind {
     public static float windMultiply = 0f;
+    public static int  ticksAdjustment = -1;
+
 
     public static float randomWindMultiply(){
         Random randW = new Random();
         windMultiply = randW.nextFloat() + 1f;
         return windMultiply;
     }
+
+    public static boolean randomWindEfficient(){
+        if(ticksAdjustment==-1 || ticksAdjustment == 1200) {
+            ticksAdjustment = 0;
+            return true;
+        }
+        else {
+            ticksAdjustment++;
+            return false;
+        }
+    }
+
+
+
 }
